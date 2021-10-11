f0dcf7d5ba1bcb9342b1d2913de1871573fe59d3
Jayna528 <72705761+Jayna528@users.noreply.github.com>
2021-10-11 18:49:08 -0400

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,20 @@
+class BlockLayer1 extends BaseClass{
+    constructor(x,y,width, height){
+      super(x,y,width,height);
+      //this.image = loadImage("sprites/wood2.png");
+     
+      var options = {
+        'restitution':0.8,
+        'friction':.3,
+        'density':1.8
+    }
+    }
+
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
+
